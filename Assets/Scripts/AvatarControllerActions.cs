6e547ae848f464aeb256f40da6a880393f5fe6ff
AxelSingleton <gosha@kolokutin.com>
2023-11-30 05:41:33 +0300

    ControllerActions added

---
+++ Assets/Scripts/AvatarControllerActions.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VRMenu.cs" company="HexDiff">
+//   Copyright (c) HexDiff, All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using UltimateXR.Avatar;
+using UltimateXR.Avatar.Controllers;
+using UltimateXR.Core;
+using UltimateXR.Core.Components.Composite;
+using UltimateXR.Devices;
+using UltimateXR.Locomotion;
+using UnityEngine;
+
+namespace VRSpaceShip.Locomotion
+{
+    public class AvatarControllerActions : MonoBehaviour
+    {
+        #region Inspector Properties/Serialized Fields
+        
+        [SerializeField] private List<ControllerEvent> _listControllerEvents = new List<ControllerEvent>();
+        
+        #endregion
+        
+        #region Public Types & Data
+        
+        public IReadOnlyList<ControllerEvent> ControllerEvents => _listControllerEvents;
+        
+        #endregion
+        
+        #region Unity
+
+        private void OnEnable()
+        {
+            UxrControllerInput.GlobalButtonStateChanged += UxrControllerInputOnGlobalButtonStateChanged;
+        }
+
+        private void OnDestroy()
+        {
+            UxrControllerInput.GlobalButtonStateChanged -= UxrControllerInputOnGlobalButtonStateChanged;
+        }
+
+        #endregion
+        
+        #region Event Handling Methods
+        
+        /// <summary>
+        ///     Called after the button have change state. Invokes invent from list <see cref="ControllerEvents"/>
+        /// </summary>
+        private void UxrControllerInputOnGlobalButtonStateChanged(object sender, UxrInputButtonEventArgs e)
+        {
+            foreach (var controllerEvent in _listControllerEvents)
+            {
+                if(e.Button == controllerEvent.Button && e.ButtonEventType == controllerEvent.ButtonEventType) controllerEvent.Invoke();
+            }
+        }
+        
+        #endregion
+    }
+}
---
+++ Assets/Scripts/ControllerEvent.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ControllerEvent.cs" company="HexDiff">
+//   Copyright (c) HexDiff, All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using UltimateXR.Core;
+using UltimateXR.Devices;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace VRSpaceShip.Locomotion
+{
+    [Serializable]
+    public class ControllerEvent
+    {
+        #region Inspector Properties/Serialized Fields
+
+        [SerializeField] private UxrInputButtons button;
+        [SerializeField] private UxrButtonEventType buttonEventType;
+        
+        [SerializeField] private UnityEvent @event;
+
+        #endregion
+
+        #region Public Types & Data
+
+        /// <summary>
+        ///     Gets or sets the button that trigger the event.
+        /// </summary>
+        public UxrInputButtons Button
+        {
+            get => button;
+            set => button = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the button event type that trigger the event.
+        /// </summary>
+        public UxrButtonEventType ButtonEventType
+        {
+            get => buttonEventType;
+            set => buttonEventType = value;
+        }
+        #endregion
+
+        #region Public Overrides object
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"Event type: button(s): {button}";
+        }
+
+        #endregion
+
+        public void Invoke()
+        {
+            @event?.Invoke();
+        }
+    }
+}
